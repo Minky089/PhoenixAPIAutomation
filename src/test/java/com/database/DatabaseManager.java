65135d6008c21a5a24323f3714e1b3aeb142d6cd
Minky <quangnt089@gmail.com>
2026-04-17 13:44:03 +0700

    Move db credential to .env file for security

--- src/test/java/com/database/DatabaseManager.java
+++ src/test/java/com/database/DatabaseManager.java
@@ -1,6 +1,7 @@
 package com.database;
 
 import com.api.utils.ConfigManager;
+import com.api.utils.EnvUtil;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import com.zaxxer.hikari.pool.HikariPool;
@@ -10,30 +11,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseManager {
-    private static final String DB_URL = ConfigManager.getProperty("DB_URL");
-    private static final String DB_USERNAME = ConfigManager.getProperty("DB_USER_NAME");
-    private static final String DB_PASSWORD = ConfigManager.getProperty("DB_PASSWORD");
+    private static final String DB_URL = EnvUtil.getValue("DB_URI");
+    private static final String DB_USERNAME = EnvUtil.getValue("DB_USERNAME");
+    private static final String DB_PASSWORD = EnvUtil.getValue("DB_PASSWORD");
     private static volatile Connection conn;
-    private static HikariConfig hikariConfig;
     private static volatile HikariDataSource hikariDataSource;
-
-    public static void createConnection() throws SQLException {
-        if (conn == null) {
-            synchronized (DatabaseManager.class) {
-                if (conn == null) {
-                    conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-                    System.out.println(conn);
-                }
-            }
-        }
-    }
 
     private static void instantiateHikariPool() {
         if (hikariDataSource == null) {
-            hikariConfig = new HikariConfig();
-            hikariConfig.setJdbcUrl(ConfigManager.getProperty("DB_URL"));
-            hikariConfig.setUsername(ConfigManager.getProperty("DB_USERNAME"));
-            hikariConfig.setPassword(ConfigManager.getProperty("DB_PASSWORD"));
+            HikariConfig hikariConfig = new HikariConfig();
+            hikariConfig.setJdbcUrl(DB_URL);
+            hikariConfig.setUsername(DB_USERNAME);
+            hikariConfig.setPassword(DB_PASSWORD);
             hikariConfig.setMaximumPoolSize(Integer.parseInt(ConfigManager.getProperty("MAXIMUM_POOL_SIZE")));
             hikariConfig.setMinimumIdle(Integer.parseInt(ConfigManager.getProperty("MINIMUM_IDLE")));
             hikariConfig.setConnectionTimeout(Integer.parseInt(ConfigManager.getProperty("CONNECTION_TIME_OUT")));
